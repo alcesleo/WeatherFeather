ced51c91d2d667a21d5725f9dc8225e65586b2af
alcesleo <lagginglion@gmail.com>
2014-01-06 13:06:13 +0100

    Initial commit

---
+++ WeatherFeather/Controllers/ForecastController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeatherFeather.Controllers
+{
+    public class ForecastController : Controller
+    {
+        //
+        // GET: /Forecast/
+
+        public ActionResult Index()
+        {
+            var data = new Webservices.YrNoWebservice().GetWeatherData();
+            return View();
+        }
+
+    }
+}
