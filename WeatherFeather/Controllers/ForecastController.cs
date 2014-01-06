3ba2f063ba1719c46c37fd6f00e54e91ffbd1972
alcesleo <lagginglion@gmail.com>
2014-01-06 13:22:31 +0100

    Some testing data, basic bootstrap grid

--- WeatherFeather/Controllers/ForecastController.cs
+++ WeatherFeather/Controllers/ForecastController.cs
@@ -13,8 +13,33 @@ namespace WeatherFeather.Controllers
 
         public ActionResult Index()
         {
-            var data = new Webservices.YrNoWebservice().GetWeatherData();
-            return View();
+            var days = new List<Models.Day>();
+            
+            days.Add(new Models.Day {
+                AirPressure = 2.3,
+                Date = DateTime.Today,
+                WindDirection = "SW",
+                Percipitation = 0.0,
+                Symbol = 4,
+                Temperature = 4.6,
+                WindSpeed = 200
+            });  
+            days.Add(new Models.Day {
+                AirPressure = 2.3,
+                Date = DateTime.Today,
+                WindDirection = "SW",
+                Percipitation = 0.0,
+                Symbol = 4,
+                Temperature = 4.6,
+                WindSpeed = 200
+            });  
+            var forecasts = new Models.Forecast {
+                Region = "Göteborg",
+                Country = "SE",
+                Location = "Lunden",
+                Days = days
+            };
+            return View("Forecast", forecasts);
         }
 
     }
---
+++ WeatherFeather/Models/Forecast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherFeather.Models
+{
+    public class Forecast
+    {
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string Location { get; set; }
+
+        public List<Day> Days { get; set; }
+    }
+}
